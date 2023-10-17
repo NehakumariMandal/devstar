0225e8d06fab46dce5fcb9b770601ed931a692fa
mkpatel-247 <meet.codes247@gmail.com>
2023-10-18 01:00:12 +0530

    Added Tax and modify calculation

--- src/routes/(tools)/invoice-generator/+page.svelte
+++ src/routes/(tools)/invoice-generator/+page.svelte
@@ -11,10 +11,11 @@
 	let intensity = 0.15;
 	let blur = 60;
 
-	let inputSets: any[] = [];
+	let inputSets: any[] = [{ item: '', rate: '', quantity: 1, additionalInfo: '', taxRate: 0 }];
 	let index = -1;
-	let showFirstPair = false;
+	let showFirstPair = true;
 	let moveDescription = false;
+	let showTaxInfo = false;
 
 	let subtotal = 0;
 	let tax = 0;
@@ -23,19 +24,26 @@
 
 	function updateTotals() {
 		subtotal = inputSets.reduce((acc, inputSet) => {
-			return acc + (inputSet.quantity * inputSet.rate || 0);
+			const itemAmount = inputSet.quantity * inputSet.rate || 0;
+			const itemTax = itemAmount * (inputSet.taxRate / 100);
+			inputSet.tax = itemTax; // Store tax for each item
+			return acc + itemAmount + itemTax;
 		}, 0);
 
-		// You can calculate tax, total, and discounts as needed here.
-		// For simplicity, I'll assume a tax rate of 10%.
-		tax = subtotal * 0.1;
+		tax = inputSets.reduce((acc, inputSet) => {
+			return acc + inputSet.tax || 0;
+		}, 0);
+
 		total = subtotal + tax;
 		balanceDue = total;
 	}
 
 	function addInputSet() {
 		index += 1;
-		inputSets = [...inputSets, { id: index, item: '', rate: '', quantity: 1, additionalInfo: '' }];
+		inputSets = [
+			...inputSets,
+			{ id: index, item: '', rate: '', quantity: 1, additionalInfo: '', taxRate: 0 }
+		];
 		showFirstPair = true;
 		moveDescription = true;
 		updateTotals();
@@ -56,17 +64,22 @@
 <section class="bg-white dark:bg-gray-900">
 	<div class="mx-auto max-w-screen-xl lg:px-12">
 		<div class="card">
-			<hr />
+			<hr class="border-t border-gray-400" />
 			<div id="invoice">
 				<table class="w-full">
-					<thead>
-						<tr class="w-full">
+					<thead class="mb-7">
+						<tr class="w-full h-10">
 							<th class="w-10" />
-							<th class="text-start w-2/5">Item Description</th>
-							<th class="text-end w-1/6 px-3">Rate</th>
-							<th class="text-end w-1/6 px-3">Quantity</th>
+							<th class="text-start w-2/5">Description</th>
+							<th class="text-end w-1/10 px-3">Rate</th>
+							<th class="text-end w-1/10 px-3">Quantity</th>
 							<th class="text-center w-fit">Amount</th>
-							<th class="text-center w-fit">Tax (%)</th>
+							<th class="text-center w-1/6">Tax(%)</th>
+						</tr>
+						<tr>
+							<td class="border-t border-black" colspan="6">
+								<hr />
+							</td>
 						</tr>
 					</thead>
 					<tbody>
@@ -75,7 +88,7 @@
 								<tr>
 									<td>
 										<button
-											class="w-10 h-10 border bg-sky-500 rounded m-3"
+											class="w-10 h-10 font-bold border text-xl bg-gray-400 rounded-lg m-3"
 											on:click={() => deleteInputSet(inputSet.id)}>X</button
 										>
 									</td>
@@ -90,7 +103,7 @@
 									</td>
 									<td class="px-2">
 										<input
-											class="w-full rounded"
+											class="w-full text-end rounded"
 											type="number"
 											min="0"
 											placeholder="0.00"
@@ -108,17 +121,22 @@
 										/>
 									</td>
 									<td>
-										<p class="flex justify-center">{inputSet.quantity * inputSet.rate}</p>
+										<p class="flex justify-center text-2xl">₹{inputSet.quantity * inputSet.rate}</p>
 									</td>
 									<td class="text-center">
-										<input class="mx-auto" type="checkbox" name="" id="" />
+										<input
+											class="w-16 text-end rounded"
+											type="number"
+											min="0"
+											bind:value={inputSet.taxRate}
+											on:change={updateTotals}
+										/>
 									</td>
 								</tr>
 								<tr>
 									<td colspan="6">
-										<input
-											class="w-2/5 py-12 ml-16 rounded"
-											type="text"
+										<textarea
+											class="w-2/5 ml-16 rounded h-36 text-start pt-2 resize-none"
 											placeholder="Additional Details"
 											bind:value={inputSet.additionalInfo}
 										/>
@@ -126,7 +144,7 @@
 								</tr>
 								{#if index !== inputSets.length - 1}
 									<tr>
-										<td colspan="6">
+										<td class="py-3" colspan="6">
 											<hr />
 										</td>
 									</tr>
@@ -135,9 +153,12 @@
 						{/each}
 					</tbody>
 				</table>
-				<hr />
 			</div>
-			<button class="w-10 h-10 border bg-sky-500 rounded m-3" on:click={addInputSet}>+</button>
+			<button
+				class="w-10 h-10 border font-bold text-3xl text-white bg-gray-600 rounded-lg shadow-lg m-3"
+				on:click={addInputSet}>+</button
+			>
+			<hr class="py-5" />
 			<div
 				class="card gap-12 items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg lg:grid lg:grid-cols-2"
 			>
